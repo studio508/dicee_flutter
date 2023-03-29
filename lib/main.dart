a40537441dd07044b2a065d9b839e6d2f7b78eab
studio508 <jy.studio508@gmail.com>
2023-03-29 14:38:11 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  // runApp(const MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // DicePage({super.key});
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+      // print('diceNumber = $leftDiceNumber');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image(
+                  image: AssetImage('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image(
+                  image: AssetImage('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
